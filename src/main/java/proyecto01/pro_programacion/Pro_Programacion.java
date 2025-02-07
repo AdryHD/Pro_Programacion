7abe31ec3dc8553932d9b307d78ec5d4465bda51
AdryHD <152119270+AdryHD@users.noreply.github.com>
2025-02-06 22:22:21 -0600

    Prueba
    prueba

---
+++ src/main/java/proyecto01/pro_programacion/Pro_Programacion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto01.pro_programacion;
+
+/**
+ *
+ * @author adryhd
+ */
+public class Pro_Programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("PRUEBA");
+       
+    }
+}
